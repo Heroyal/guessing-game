2eb3caafa9024584c358a5b17b36d9abb0653a66
Heroyal <1669100172@qq.com>
2020-03-30 17:06:32 +0800

    second version,only five times

--- guess.py
+++ guess.py
@@ -1,12 +1,14 @@
 from random import randint
 def main():
     num=randint(1,10)
-    while True:
+    a=0
+    while a<5:
         guess=input('Plaease give a number between on and ten: ')
         if int(guess)==num:
             print('Yor are right!')
             break
         else:
             print('Sorry,that\'s wrong,please guess again!')
+            a+=1
 if __name__=='__main__':
     main()
